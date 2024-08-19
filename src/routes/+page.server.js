b69942cc5d0dc7d678c96399db1e3a7c5da5fc89
Average Blank <112507212+AverageBlank@users.noreply.github.com>
2024-08-19 11:54:28 +0530

    Adding Files

---
+++ src/routes/+page.server.js
@@ -0,0 +1,8 @@
+export const load = async ({ locals: { supabase } }) => {
+	const { data: countries } = await supabase
+		.from('countries')
+		.select('name')
+		.limit(5)
+		.order('name');
+	return { countries: countries ?? [] };
+};
